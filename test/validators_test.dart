3d5f912e5c9a37c1ea7c8d4405e45c7a0d678668
Shuaib Albastaki <stargazingwiz@gmail.com>
2022-07-07 14:39:10 +0400

    added validators unit tests

---
+++ test/validators_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/validators.dart';
+
+void main() {
+  test("Non empty String", () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid("test"), true);
+  });
+  test("Empty String", () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(""), false);
+  });
+}

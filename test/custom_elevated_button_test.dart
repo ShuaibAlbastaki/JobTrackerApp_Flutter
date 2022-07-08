b7f1e728bdabaaa6f5a47b356dafcf7513eed3f5
Shuaib Albastaki <stargazingwiz@gmail.com>
2022-07-08 11:44:57 +0400

    added more tests

--- test/custom_elevated_button_test.dart
+++ test/custom_elevated_button_test.dart
@@ -1,9 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_flutter_course/app/common_widgets/custom_elevated_button.dart';
 
 void main() {
   testWidgets("", (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: CustomElevatedButton()));
+    await tester.pumpWidget(MaterialApp(
+      home: CustomElevatedButton(
+        child: Text("tap me"),
+      ),
+    ));
+    final button = find.byType(ElevatedButton);
+    expect(button, findsOneWidget);
+    expect(find.byType(RaisedButton), findsNothing);
+    expect(find.text("tap me"), findsOneWidget);
   });
 }
